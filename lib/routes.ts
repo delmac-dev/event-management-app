3b23d5650ddfa3e74aa25177b9ac998a26e83158
delmac <delalitengue.dev@gmail.com>
2024-06-10 15:49:28 +0000

    made edits to routes

--- lib/routes.ts
+++ lib/routes.ts
@@ -14,25 +14,25 @@ export const _ticket = (ticketCode: string) => `/ticket/${ticketCode}`;
 export const _profile = (profileId: string) => `/profile/${profileId}`;
 
 // private
-export const _dashboard = (profileId: string) => `/dashboard/${profileId}`;
+export const _dashboard = `/dashboard`;
 
-export const _dashboardEvents = (profileId: string) => `${_dashboard(profileId)}/events`;
-export const _dashboardEvent = (profileId: string, eventId: string) => `${_dashboardEvents(profileId)}/${eventId}`;
-export const _dashboardEventAttendees = (profileId: string, eventId: string) => `${_dashboardEvent(profileId, eventId)}/attendees`;
-export const _dashboardEventModerators = (profileId: string, eventId: string) => `${_dashboardEvent(profileId, eventId)}/moderators`;
-export const _dashboardEventRoles = (profileId: string, eventId: string) => `${_dashboardEvent(profileId, eventId)}/roles`;
-export const _dashboardEventTickets = (profileId: string, eventId: string) => `${_dashboardEvent(profileId, eventId)}/tickets`;
+export const _dashboardEvents = `${_dashboard}/events`;
+export const _dashboardEvent = (eventId: string) => `${_dashboardEvents}/${eventId}`;
+export const _dashboardEventAttendees = (eventId: string) => `${_dashboardEvent(eventId)}/attendees`;
+export const _dashboardEventModerators = (eventId: string) => `${_dashboardEvent(eventId)}/moderators`;
+export const _dashboardEventRoles = (eventId: string) => `${_dashboardEvent(eventId)}/roles`;
+export const _dashboardEventTickets = (eventId: string) => `${_dashboardEvent(eventId)}/tickets`;
 
-export const _dashboardOrgs = (profileId: string) => `${_dashboard(profileId)}/organisations`;
-export const _dashboardOrg = (profileId: string, orgId: string) => `${_dashboardOrgs(profileId)}/${orgId}`;
-export const _dashboardOrgMembers = (profileId: string, orgId: string) => `${_dashboardOrg(profileId, orgId)}/members`;
-export const _dashboardOrgEvents = (profileId: string, orgId: string) => `${_dashboardOrg(profileId, orgId)}/events`;
-export const _dashboardOrgRoles = (profileId: string, orgId: string) => `${_dashboardOrg(profileId, orgId)}/roles`;
+export const _dashboardOrgs = `${_dashboard}/organisations`;
+export const _dashboardOrg = (orgId: string) => `${_dashboardOrgs}/${orgId}`;
+export const _dashboardOrgMembers = (orgId: string) => `${_dashboardOrg(orgId)}/members`;
+export const _dashboardOrgEvents = (orgId: string) => `${_dashboardOrg(orgId)}/events`;
+export const _dashboardOrgRoles = (orgId: string) => `${_dashboardOrg(orgId)}/roles`;
 
-export const _dashboardTickets = (profileId: string) => `${_dashboard(profileId)}/tickets`;
-export const _dashboardProfile = (profileId: string) => `${_dashboard(profileId)}/profile`;
-export const _dashboardNotifications = (profileId: string) => `${_dashboard(profileId)}/notifications`;
-export const _dashboardSettings = (profileId: string) => `${_dashboard(profileId)}/settings`;
+export const _dashboardTickets = `${_dashboard}/tickets`;
+export const _dashboardProfile = `${_dashboard}/profile`;
+export const _dashboardNotifications = `${_dashboard}/notifications`;
+export const _dashboardSettings = `${_dashboard}/settings`;
 
 // company
 export const _company = '/company';
