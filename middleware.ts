d626dc79366fbdabb0e307f7b8daffea0705760e
delmac <delalitengue.dev@gmail.com>
2024-06-15 13:36:58 +0000

    updated middleware to protect private routes

--- middleware.ts
+++ middleware.ts
@@ -5,23 +5,21 @@ import { _dashboard, _events, _home, _join, _login } from "./lib/routes";
 export async function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
   const pathname = url.pathname;
-  const isLogin = pathname.startsWith(_login);
-  const isJoin = pathname.startsWith(_join);
+  const isAuth = pathname.startsWith(_login) || pathname.startsWith(_join);
   const isPublic = (pathname: string) => /^\/(events|organisations|tickets|profile)|\/$/.test(pathname);
   const { user, response } = await updateSession(request);
 
+  // logged out users can only access public pages or auth pages
   if(!user){
-    // logged out users can only access public pages or auth pages
-    if(isPublic(pathname) || isJoin || isLogin)
-      return response;
+    if(isPublic(pathname) || isAuth) return response;
     return NextResponse.redirect(new URL(_login, url.origin));
-  }else {
-    // signed in users cant access login or join page
-    if(isLogin || isJoin)
-      return NextResponse.redirect(new URL(_dashboard, url.origin))
-    return response;
   }
 
+  // signed in users cant access login or join page
+  if(isAuth) return NextResponse.redirect(new URL(_dashboard, url.origin));
+
+  return response;
+  
 }
 
 export const config = {
