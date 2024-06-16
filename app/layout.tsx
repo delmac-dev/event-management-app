38dcffcdb5c7f697b54bdc2004e58e6d730886ff
delmac <delalitengue.dev@gmail.com>
2024-06-16 15:01:32 +0000

    sent common components to common folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/common/theme-provider"
 import { Metadata } from "next";
 import "./globals.css";
 
