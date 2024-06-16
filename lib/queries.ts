ff5dd741a03b2914be130bcd89e1e42ba51534c9
delmac <delalitengue.dev@gmail.com>
2024-06-16 12:22:33 +0000

    added query functions

---
+++ lib/queries.ts
@@ -0,0 +1,58 @@
+
+// ALL QUERIES RELATING TO EVENTS
+export const getEvents = () => {};
+export const getEventByID = () => {};
+export const setEvent = () => {};
+export const updateEvent = () => {};
+
+export const getEventTickets = () => {};
+export const setEventTicket = () => {};
+export const updateEventTicket = () => {};
+export const getEventTicketByID = () => {};
+
+export const getEventAttendees = () => {};
+export const setEventAttendee = () => {};
+export const updateEventAttendee = () => {}; // for checking an attendee in 
+export const deleteEventAttendee = () => {};
+
+export const getEventModerators = () => {};
+export const setEventModerator = () => {};
+export const updateEventModerator = () => {};
+export const removeEventModerator = () => {};
+
+export const getEventRoles = () => {};
+export const setEventRole = () => {};
+export const updateEventRole = () => {};
+export const removeEventRole = () => {};
+
+// ALL QUERIES RELATING TO ORGANIZATIONS
+export const getOrganisations = () => {};
+export const getOrganisationByID = () => {};
+export const setOrganisation = () => {};
+export const updateOganisation = () => {};
+
+export const getOrganisationRoles = () => {};
+export const setOrganisationRole = () => {};
+export const updateOrganisationRole = () => {};
+export const removeOrganisationRole = () => {};
+
+export const getOrgansationMembers = () => {};
+export const setOrganisationMember = () => {};
+export const updateOrganisationMember = () => {};
+export const removeOrganisationMember = () => {};
+
+// ALL QUERIES RELATING TO TICKETS
+export const getTicketByCode = () => {};
+export const getTickets = () => {};
+export const unbookTicket = () => {};
+
+// ALL QUERIES RELATING TO PROFILE
+export const getProfile = () => {};
+export const updateProfile = () => {};
+export const setProfile = () => {};
+
+export const getNotifications = () => {};
+export const updateNotification = () => {};
+
+export const getSettings = () => {};
+export const updateSettings = () => {};
