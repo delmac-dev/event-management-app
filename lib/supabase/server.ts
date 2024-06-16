18834418d5de1c22bd54a60e00a4828eec8dfd77
delmac <delalitengue.dev@gmail.com>
2024-06-16 10:53:32 +0000

    editted server client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -11,25 +11,7 @@ export const createClient = () => {
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value: "", ...options });
-          } catch (error) {
-            // The `delete` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
+        }
       },
     },
   );
