a7aa618ba1267729c5d2c89499c7683c9975df21
delmac <delalitengue.dev@gmail.com>
2024-06-17 13:01:31 +0000

    changed theme to light

--- app/(public)/(home)/page.tsx
+++ app/(public)/(home)/page.tsx
@@ -5,6 +5,12 @@ export default async function Index() {
   return (
     <>
       <Header />
+      <section>hero section</section>
+      <section>features section</section>
+      <section>why use us section</section>
+      <section>faq section</section>
+      <section>try us out section</section>
+      <footer>footer</footer>
     </>
   );
 }
