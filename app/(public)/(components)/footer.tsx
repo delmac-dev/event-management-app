f5be3f52015b4f847973ddf27bc83976429df2a3
delmac <delalitengue.dev@gmail.com>
2024-06-18 09:15:08 +0000

    editted footer

--- app/(public)/(components)/footer.tsx
+++ app/(public)/(components)/footer.tsx
@@ -1,6 +1,29 @@
+import Logo from "@/components/common/logo";
+import { _dashboard, _events, _home, _organisations, _tickets } from "@/lib/routes";
+
+const footerLinks = [
+    {
+        title: "General",
+        links: [
+            {name: "home", link: _home},
+            {name: "events", link: _events},
+            {name: "organisations", link: _organisations},
+            {name: "my dashboard", link: _dashboard},
+            {name: "find my ticket", link: _tickets},
+        ]
+    }
+]
+
 const Footer = () => {
   return (
-    <footer>footer</footer>
+    <footer className="w-full py-8 px-2 flex bg-secondary justify-center">
+        <div className="">
+            <div className="flex gap-2 5 items-center flex-col">
+                <Logo />
+                <span className="text-lg font-semibold">CampusEvents</span>
+            </div>
+        </div>
+    </footer>
   )
 }
 
