d8de18a8f3d597495dcfc4eaa3f8d552f9e01d0b
delmac <delalitengue.dev@gmail.com>
2024-06-29 12:13:33 +0000

    made edits to server ts file

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,18 +1,24 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-export const createClient = () => {
+export function createClient() {
   const cookieStore = cookies();
-
+  
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value;
-        }
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.delete({ name, ...options })
+        },
       },
-    },
+    }
   );
-};
+}
