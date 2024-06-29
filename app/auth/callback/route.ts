770eb0ca137787eb5dfc897064be1387f359ef18
delmac <delalitengue.dev@gmail.com>
2024-06-29 11:30:33 +0000

    made changes to middleware

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,5 +1,6 @@
 import { _dashboard, _login } from "@/lib/routes";
-import { createClient } from "@/lib/supabase/server";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -8,7 +9,24 @@ export async function GET(request: Request) {
   const next = searchParams.get('next') ?? _dashboard;
 
   if (code) {
-    const supabase = createClient();
+    const cookieStore = cookies()
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options })
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.delete({ name, ...options })
+          },
+        },
+      }
+    );
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`)
