4e449334c5c13e513554e778b54d2ca7a3180349
delmac <delalitengue.dev@gmail.com>
2024-06-30 09:42:51 +0000

    made sure there is session before adding to authorization header

--- lib/client.ts
+++ lib/client.ts
@@ -1,18 +1,20 @@
-import { createClient as _createClient } from "@supabase/supabase-js";
+import { SupabaseClientOptions, createClient as _createClient } from "@supabase/supabase-js";
 import { auth } from "@/auth";
 
 export const createClient = async () => {
   const session = await auth();
 
+  const options:SupabaseClientOptions<"public"> = {
+    global: {
+      headers: {
+        Authorization: `Bearer ${session?.supabaseAccessToken}`,
+      },
+    },
+  }
+
   return _createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      global: {
-        headers: {
-          Authorization: `Bearer ${session?.supabaseAccessToken}`,
-        },
-      },
-    }
+    session? options : undefined
   );
 };
