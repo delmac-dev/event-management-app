40a329d7be1206ffd18a62625b70da21dbcede24
delmac <delalitengue.dev@gmail.com>
2024-06-30 16:15:41 +0000

    added supabase client setup

--- app/(public)/(components)/header.tsx
+++ app/(public)/(components)/header.tsx
@@ -1,35 +1,27 @@
+"use client";
+
 import Logo from "@/components/common/logo";
 import { Button } from "@/components/ui/button";
+import { signOut } from "@/lib/actions";
 import { _dashboard, _events, _home, _organisations, _tickets } from "@/lib/routes";
+import { createClient } from "@/lib/supabase/client";
 import { cn } from "@/lib/utils";
 import { User } from "@supabase/supabase-js";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const supabase = createClient();
 
 const navLinks = [
-    {
-        name: "home",
-        link: _home
-    },
-    {
-        name: "events",
-        link: _events
-    },
-    {
-        name: "organisations",
-        link: _organisations
-    },
-    {
-        name: "dashboard",
-        link: _dashboard
-    },
-    {
-        name: "find my ticket",
-        link: _tickets
-    }
+    { name: "home", link: _home },
+    { name: "events",link: _events },
+    { name: "organisations", link: _organisations },
+    { name: "dashboard", link: _dashboard },
+    { name: "find my ticket", link: _tickets }
 ]
 
-export default function Header ({user}:{user?: User}) {
+export default function Header () {
 
     return (
         <header className="relative w-full h-12 flex_center justify-between px-5 md:px-10">
@@ -45,18 +37,37 @@ export default function Header ({user}:{user?: User}) {
     )
 }
 
-const AuthSection = ({user, isMobile = false}:{user?: User, isMobile?: boolean}) => (
-    user ? (
-        <div className="flex gap-2">
-            user goes here
-        </div>
-    ): (
-        <div className={cn(!isMobile? "hidden md:flex gap-2" : "flex gap-2 flex-col md:hidden")}>
-            <Button variant={'outline'} size='xs'>Login</Button>
-            <Button size='xs'>Sign Up</Button>
-        </div>
+const AuthSection = ({isMobile = false}:{ isMobile?: boolean }) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const { data, error } = await supabase.auth.getUser();
+            if (error) {
+                console.error('Error fetching user:', error);
+                return;
+            }
+            setUser(data.user);
+        };
+        
+        fetchUser();
+    },[]); 
+
+    return (
+        <>
+            {user ? (
+                <form className="flex gap-2">
+                    <Button size='xs' formAction={async () => await signOut()}> Sign Out</Button>
+                </form>
+            ): (
+                <div className={cn(!isMobile? "hidden md:flex gap-2" : "flex gap-2 flex-col md:hidden")}>
+                    <Button variant={'outline'} size='xs'>Login</Button>
+                    <Button size='xs'>Sign Up</Button>
+                </div>
+            )}
+        </>
     )
-)
+}
 
 const MobileNav = () => {
     return (
