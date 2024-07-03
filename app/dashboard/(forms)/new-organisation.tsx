7c57f7737e65f9c56aca535b42c173402ccef499
delmac <delalitengue.dev@gmail.com>
2024-07-03 08:22:32 +0000

    added new organisation form

---
+++ app/dashboard/(forms)/new-organisation.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useEffect, useState } from "react";
+
+export function NewOrganisationModal({ isOpen }:{ isOpen: boolean }) {
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        setOpen(isOpen);
+      }, [isOpen]);
+
+    return (
+        <Dialog open = {open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button size="xs">New Organisation</Button>
+            </DialogTrigger>
+            <DialogContent  className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Create A New Organisation</DialogTitle>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Name
+                        </Label>
+                        <Input id="name" className="col-span-3" />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Username
+                        </Label>
+                        <Input id="username" className="col-span-3" />
+                    </div>
+                </div>
+                <DialogFooter className="justify-center">
+                    <Button type="submit" size="xs">Create Organisation</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
