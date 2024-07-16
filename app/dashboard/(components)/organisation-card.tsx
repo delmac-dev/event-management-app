07b7d07455c9b1fca1b35cd0600d549c868917bf
delmac <delalitengue.dev@gmail.com>
2024-07-16 01:07:40 +0000

    made edit to org card

--- app/dashboard/(components)/organisation-card.tsx
+++ app/dashboard/(components)/organisation-card.tsx
@@ -2,16 +2,35 @@
 
 import { OrgType } from '@/lib/constants';
 import { _dashboardOrgEvents } from '@/lib/routes';
+import { cn } from '@/lib/utils';
+import { ChevronLeft, ChevronRight, Image } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react'
 
-const OrganisationCard = ({id, name}: OrgType) => {
+const OrganisationCard = ({id, name, headline, about}: OrgType) => {
   return (
-    <div className="w-full bg-blue-500 h-16 rounded-sm p-2.5">
-        <Link href={_dashboardOrgEvents(id)} className="block w-full text-white overflow-hidden whitespace-nowrap overflow-ellipsis">
-            {name}
-        </Link>
-        {/* some more details and edit and delete and show buttons */}
+    <div className="w-full h-44 rounded-sm flex flex-col border gap-3">
+        {/* href={_dashboardOrgEvents(id)} */}
+        <div className="w-full flex gap-2 px-4 pt-4">
+          <div className="size-10 rounded-full bg-muted flex_center">
+            <Image className='size-5 text-muted-foreground/60' />
+          </div>
+          <div className="flex-1 h-full flex flex-col justify-center">
+            <h5 className="text-sm font-medium max-w-full overflow-hidden text-ellipsis text-nowrap">{name}</h5>
+            <p className="text-xs text-muted-foreground mt-1 5">{headline}</p>
+          </div>
+        </div>
+        <div className="w-full flex-1 px-4">
+          <p className="text-sm text-muted-foreground overflow-hidden text-ellipsis line-clamp-2">{about}</p>
+        </div>
+        <div className="w-full h-10 bg-muted/40 border-t px-4 flex items-center justify-between">
+          <div className="flex ">
+            <div className="flex items-center">
+              {Array(3).fill("").map((_, _id)=>(<div key={_id} className={cn("relative size-6 -ml-3 first:ml-0 rounded-full shadow-md bg-background")}></div>))}
+            </div>
+          </div>
+          <ChevronRight className='size-5 text-muted-foreground'/>
+        </div>
     </div>
   )
 }
