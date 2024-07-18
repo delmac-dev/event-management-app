ba41319d189ad62a597d094546c1a18aad662b23
delmac <delalitengue.dev@gmail.com>
2024-07-18 12:18:18 +0000

    added tables to neccessary tabs in org route

--- app/dashboard/organisations/[organisation_id]/members/page.tsx
+++ app/dashboard/organisations/[organisation_id]/members/page.tsx
@@ -1,8 +1,10 @@
 import BodyContent from "@/app/dashboard/(components)/body-content";
 import BodyHeader from "@/app/dashboard/(components)/body-header";
+import { DataTable } from "@/components/common/data-table";
 import { Button } from "@/components/ui/button";
 import { QueryProps } from "@/lib/types";
 import { ArchiveX } from "lucide-react";
+import { columns } from "./columns";
 
 export default async function OrganisationMembers({ params }: QueryProps) {
   const organisationID = params.organisation_id;
@@ -13,11 +15,7 @@ export default async function OrganisationMembers({ params }: QueryProps) {
         <h2 className="text-xl font-medium">Organisation Members</h2>
       </BodyHeader>
       <BodyContent>
-        <div className="w-full h-64 rounded-lg border border-dashed border-spacing-4 flex_center flex-col gap-3 text-sm font-medium text-secondary-foreground">
-          <ArchiveX />
-          No Members Yet
-          <Button size='xs'>Add a member</Button>
-        </div>
+        <DataTable columns={columns} data={[]} />
       </BodyContent>
     </>
   );
--- app/dashboard/organisations/[organisation_id]/roles/page.tsx
+++ app/dashboard/organisations/[organisation_id]/roles/page.tsx
@@ -3,6 +3,8 @@ import BodyHeader from "@/app/dashboard/(components)/body-header";
 import { Button } from "@/components/ui/button";
 import { QueryProps } from "@/lib/types";
 import { ArchiveX } from "lucide-react";
+import { columns } from "./columns";
+import { DataTable } from "@/components/common/data-table";
 
 export default async function OrganisationRoles({ params }: QueryProps) {
   const organisationID = params.organisation_id;
@@ -13,11 +15,7 @@ export default async function OrganisationRoles({ params }: QueryProps) {
         <h2 className="text-xl font-medium">Organisation Roles</h2>
       </BodyHeader>
       <BodyContent>
-        <div className="w-full h-64 rounded-lg border border-dashed border-spacing-4 flex_center flex-col gap-3 text-sm font-medium text-secondary-foreground">
-          <ArchiveX />
-          No Roles Yet
-          <Button size='xs'>Add a role</Button>
-        </div>
+      <DataTable columns={columns} data={[]} />
       </BodyContent>
     </>
   );
