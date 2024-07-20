0c834ff46ac77fa39cff8f354e9fbf9e2e4c1762
delmac <delalitengue.dev@gmail.com>
2024-07-20 01:48:47 +0000

    made edits

--- app/dashboard/(form-handlers)/moderator-handler.tsx
+++ app/dashboard/(form-handlers)/moderator-handler.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import SheetFormWrapper from "@/components/common/sheet-form-wrapper";
+import HandleModeratorForm from "@/components/forms/handle-moderator";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 
 type ModeratorHandlerProps = {
     isOpen: boolean,
     title: string,
-    description?: string
+    description?: string,
+    eventID: string
 }
 
 export default function ModeratorHandler(props: ModeratorHandlerProps){
     const [open, setOpen] = useState(false);
-    const { title, isOpen, description } = props;
+    const { title, isOpen, description, eventID } = props;
     const wrapperData = { open, setOpen, title, description};
 
     useEffect(() => {
@@ -23,7 +25,7 @@ export default function ModeratorHandler(props: ModeratorHandlerProps){
         <>
             <Button size="xs" onClick={() => setOpen(true)}>Add a moderator</Button>
             <SheetFormWrapper { ...wrapperData } className="">
-                <></>
+                <HandleModeratorForm  eventID={eventID} closeHandler={()=>{}} />
             </SheetFormWrapper>
         </>
     )
