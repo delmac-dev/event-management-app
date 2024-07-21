23014d162b451e9cd07a06546164ab673943a2ef
delmac <delalitengue.dev@gmail.com>
2024-07-21 17:57:17 +0000

    made edits

--- lib/types.ts
+++ lib/types.ts
@@ -19,20 +19,6 @@ export type NavigationProps = {
     active: boolean;
 };
 
-export type ProfileProps = Database['public']['Tables']['profiles']['Row'];
-
-export type EventProps = Database['public']['Tables']['events']['Row'];
-
-export type OrgProps = Database['public']['Tables']['organisations']['Row'];
-
-export type OrgMemberProps = Database['public']['Tables']['organisation_members']['Row'];
-
-export type AttendeeProps = Database['public']['Tables']['attendees']['Row'];
-
-export type TicketProps = Database['public']['Tables']['tickets']['Row'];
-
-export type NotificationProps = Database['public']['Tables']['notifications']['Row'];
-
 export type FetchedOrganisationProps = {
     about: string,
     avatar_url: string,
