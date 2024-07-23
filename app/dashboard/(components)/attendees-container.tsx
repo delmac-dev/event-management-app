b930097fc9157958ff9795259c85a8f51d353157
delmac <delalitengue.dev@gmail.com>
2024-07-23 05:33:02 +0000

    added an attendees-container component

---
+++ app/dashboard/(components)/attendees-container.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { DataTable } from "@/components/common/data-table";
+import BodyContent from "./body-content";
+import { columns } from "../events/[event_id]/attendees/columns";
+import { useGetEventAttendees } from "@/lib/query-hooks";
+import SpinnerIcon from "@/components/icons/spinner-icon";
+
+export default function AttendeesContainer({eventID}:{eventID: string}) {
+    const { data: attendees, isLoading } = useGetEventAttendees(eventID);
+
+    if(isLoading) {
+        return (
+            <BodyContent className="flex_center">
+                <SpinnerIcon className="size-10 text-secondary-foreground" />
+            </BodyContent>
+        )
+    }
+
+    return (
+        <BodyContent>
+            <DataTable columns={columns} data={[]} />
+        </BodyContent>
+    )
+}
