abbe3e00f00817f414bc25eca521f8a095eabb6e
delmac <delalitengue.dev@gmail.com>
2024-07-27 19:29:26 +0000

    started edit to notification

--- components/common/notification-button.tsx
+++ components/common/notification-button.tsx
@@ -3,14 +3,20 @@
 import { Button } from "../ui/button";
 import { Bell } from "lucide-react";
 import { _dashboardNotifications } from "@/lib/routes";
-import { useRouter } from "next/navigation";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 export default function Notifications() {
-    const router = useRouter();
 
     return (
-        <Button variant='outline' size='sm' className="aspect-square p-1.5 rounded-full" onClick={() => router.push(_dashboardNotifications)}>
-            <Bell size={20} />
-        </Button>
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant='outline' size='sm' className="aspect-square p-1.5 rounded-full">
+                    <Bell size={20} />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent sideOffset={14} className="w-56 flex flex-col mr-4 p-0">
+````
+            </PopoverContent>
+        </Popover>
     )
 }
