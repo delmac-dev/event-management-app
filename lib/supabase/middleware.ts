f02c8284337da6f10425e8d9b48339428d86ccff
delmac <delalitengue.dev@gmail.com>
2024-07-30 22:58:30 +0000

    made fixes to auth

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -3,33 +3,29 @@ import { type NextRequest, NextResponse } from "next/server";
 
 export const updateSession = async (request: NextRequest) => {
 
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
+  let response = NextResponse.next({request})
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
+        getAll() {
+          return request.cookies.getAll()
         },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({ name, value, ...options });
-          response = NextResponse.next({ request: { headers: request.headers } });
-          response.cookies.set({ name, value, ...options });
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({ name, value: "", ...options });
-          response = NextResponse.next({ request: { headers: request.headers } });
-          response.cookies.set({ name, value: "", ...options });
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+          response = NextResponse.next({
+            request,
+          })
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options)
+          )
         },
       },
-    },
-  );
+    }
+  )
+
 
   const { data: { user }} = await supabase.auth.getUser();
 
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "./lib/supabase/middleware";
-import { _dashboard,  _dashboardOrgs,  _dashboardProfile,  _dashboardProfileEdit,  _login } from "./lib/routes";
+import { _dashboard, _dashboardOrgs, _dashboardProfile, _dashboardProfileEdit, _login } from "./lib/routes";
 
 export async function middleware(request: NextRequest) {
   const { response, user } = await updateSession(request);
@@ -9,26 +9,26 @@ export async function middleware(request: NextRequest) {
   const pathname = url.pathname;
   const params = url.searchParams;
   const hasParams = params.size != 0;
-  const redirectTo = encodeURIComponent(`${pathname}${hasParams? `?${params.toString()}`: ''}`);
+  const redirectTo = encodeURIComponent(`${pathname}${hasParams ? `?${params.toString()}` : ''}`);
   const isAuth = pathname.startsWith(_login);
   const isProtected = pathname.startsWith(_dashboard);
   const isPublic = /^\/(events|organisations|tickets|profile|api)|\/$/.test(pathname);
 
-  // logged out users cant access protected pages
-  if(!user && isProtected && !isAuth)
-    return Response.redirect(new URL(`${_login}?redirectTo=${redirectTo}`, url.origin));
+  // logged out users can't access protected pages
+  if (!user && isProtected && !isAuth)
+    return NextResponse.redirect(new URL(`${_login}?redirectTo=${redirectTo}`, url.origin));
 
   // _dashboard is not accessible
-  if(pathname === _dashboard)
-    return Response.redirect(new URL(_dashboardOrgs, url.origin)); 
+  if (pathname === _dashboard)
+    return NextResponse.redirect(new URL(_dashboardOrgs, url.origin));
 
   // _dashboardProfile is not accessible
-  if(pathname === _dashboardProfile)
-    return Response.redirect(new URL(_dashboardProfileEdit, url.origin)); 
+  if (pathname === _dashboardProfile)
+    return NextResponse.redirect(new URL(_dashboardProfileEdit, url.origin));
 
-  // signed in users cant access login or join page
-  if(user && isAuth)
-    return Response.redirect(new URL(_dashboard, url.origin));
+  // signed-in users can't access login or join page
+  if (user && isAuth)
+    return NextResponse.redirect(new URL(_dashboard, url.origin));
 
   return response;
 }
