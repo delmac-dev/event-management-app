00f512c26b113f99d96fd42cb6616e6ecfce0b62
delmac <delalitengue.dev@gmail.com>
2024-07-31 02:54:06 +0000

    made fixes 5.0

--- app/(public)/(components)/header.tsx
+++ app/(public)/(components)/header.tsx
@@ -59,6 +59,8 @@ const HeaderOptions = () => {
     useEffect(()=> {
         const getUser = async() => {
             const { data: { user: authUser }, error} = await supabase.auth.getUser();
+            console.log(user?.user_metadata["full_name"]);
+            
             
             if(error) setUser(null);
             else setUser(authUser);
